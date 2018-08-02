759159c794bdfb7a6cd6a0f7fcd40408a23631d1
NeXT <sunxufei@hotmail.com>
2018-08-03 05:16:55 +0800

    with libraries

--- maven-docker/src/main/java/help/App.java
+++ maven-docker/src/main/java/help/App.java
@@ -1,13 +1,25 @@
 package help;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+// set DOCKER_HOST=tcp://???IP:2375
+
+@SpringBootApplication
+@RestController
 public class App 
 {
+	@RequestMapping("/test")
+	public String test()
+	{
+		return "Hello Docker";
+	}
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	System.out.println("Hello World");
+    	SpringApplication.run(App.class, args);
     }
 }
