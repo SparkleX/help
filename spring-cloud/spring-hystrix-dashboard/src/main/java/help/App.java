3f3d02b291da4e701283cbc662f385a3c4eb4c58
SparkleX <sunxufei@hotmail.com>
2018-08-09 13:38:01 +0800

    dashboard

---
+++ spring-cloud/spring-hystrix-dashboard/src/main/java/help/App.java
@@ -0,0 +1,32 @@
+package help;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@SpringBootApplication
+@EnableCircuitBreaker
+@RestController
+@EnableHystrixDashboard
+public class App 
+{
+	@Autowired
+	TestComponent component;
+	@RequestMapping("/test")
+	public BigDecimal test() throws Exception
+	{
+		return component.getDiscount("ItemA");
+	}
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(App.class, args);
+	}
+}
