ac2c363f11d4e019f4da1dbf8b9c6d87783e4f8f
SparkleX <sunxufei@hotmail.com>
2018-08-09 13:45:47 +0800

    add

--- spring-feign/src/main/java/help/client/openfeign/Application.java
+++ spring-feign/src/main/java/help/client/openfeign/Application.java
@@ -1,9 +1,6 @@
 package help.client.openfeign;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.feign.EnableFeignClients;
-
 import feign.Feign;
 import feign.gson.GsonDecoder;
 import feign.gson.GsonEncoder;
@@ -11,7 +8,6 @@ import feign.okhttp.OkHttpClient;
 import feign.slf4j.Slf4jLogger;
 
 @SpringBootApplication
-//@EnableFeignClients
 public class Application {
 
     public static void main(String[] args) 
@@ -25,7 +21,6 @@ public class Application {
     			  .target(Client.class, "http://localhost:8080");
     	ClientBean bean = bookClient.test();
     	System.out.println(bean.getCode());
-       // SpringApplication.run(Application.class, args);
     }
 
 }
--- spring-feign/src/main/java/help/client/openfeign/Client.java
+++ spring-feign/src/main/java/help/client/openfeign/Client.java
@@ -1,15 +1,10 @@
 package help.client.openfeign;
 
-//import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import feign.RequestLine;
 
-//@FeignClient("stores")
 public interface Client 
 {
-    //@RequestMapping(method = RequestMethod.GET, value = "/test")
 	@RequestLine("GET /test")
     ClientBean test();
 }
