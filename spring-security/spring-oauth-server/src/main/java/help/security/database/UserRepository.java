ae1d06a5468b426d7322ffbe3699bdb0d1d96d61
SparkleX <sunxufei@hotmail.com>
2018-08-23 16:53:02 +0800

    move to other repo

--- spring-security/spring-oauth-server/src/main/java/help/security/database/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package help.security.database;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, String> 
-{
-	 
-    Users findByUsername(String username);
-}
