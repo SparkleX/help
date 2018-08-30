bfca668b1f2a6f9d574d2b140257f4b1953055a2
NeXT <sunxufei@hotmail.com>
2018-08-30 23:15:55 +0800

    temp

--- java-lang/log/src/main/java/log/SpringBootApp.java
+++ java-lang/log/src/main/java/log/SpringBootApp.java
@@ -2,20 +2,23 @@ package log;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class Test {
+
+@SpringBootApplication
+public class SpringBootApp 
+{
 
 	public static void main(String[] args) 
 	{
+		SpringApplication.run(SpringBootApp.class, args);
 		//https://www.cnblogs.com/DeepLearing/p/5663178.html
-		//http://logback.qos.ch/manual/layouts.html#coloring
         Logger logger = LoggerFactory.getLogger(Test.class);
         logger.info("info........");
         logger.warn("warn........");
         logger.debug("debug........");
         logger.trace("trace........");
         logger.error("error........");
-        ch.qos.logback.core.ConsoleAppender a;
 	}
-
 }
