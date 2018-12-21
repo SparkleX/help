15fd0e5b3d62a6b93002a2fd87d6af3e720096b3
NeXT <sunxufei@hotmail.com>
2018-12-22 00:12:41 +0800

    mock server

---
+++ mock/mock-http-server/src/test/java/help/TestMockServer.java
@@ -0,0 +1,31 @@
+package help;
+
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+
+import org.mockserver.configuration.ConfigurationProperties;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.model.Header;
+
+public class TestMockServer 
+{
+	public static void main(String[] argv) 
+	{
+		ConfigurationProperties.overrideLogLevel("ERROR");
+		
+		ClientAndServer mockServer = ClientAndServer.startClientAndServer(8080);
+		
+		mockServer.when(request().withMethod("GET").withPath("/test"))
+				.respond(response().withStatusCode(200)
+						.withHeaders(new Header("Content-Type", "application/json; charset=utf-8"),
+								new Header("Cache-Control", "public, max-age=86400"))
+						.withBody("test"));
+		
+		mockServer.when(request().withMethod("GET").withPath("/aaaa"))
+				.respond(response().withStatusCode(200)
+						.withHeaders(new Header("Content-Type", "application/json; charset=utf-8"),
+								new Header("Cache-Control", "public, max-age=86400"))
+						.withBody("aaaa"));
+
+	}
+}
