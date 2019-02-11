d0c02d2b5fdb7ece967ef93b87fa9105886529b6
SparkleX <sunxufei@hotmail.com>
2019-02-11 17:47:42 +0800

    add

--- maven-plugin/maven-plugin-gen/src/main/java/help/sourcegen/CodeGen.java
+++ maven-plugin/maven-plugin-gen/src/main/java/help/sourcegen/CodeGen.java
@@ -1,4 +1,4 @@
-package com.mook.plugin.gr_maven_plugin;
+package help.sourcegen;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,30 +10,39 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 
-@Mojo(name = "gen")//, defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+@Mojo(name = "gen", requiresDependencyResolution = ResolutionScope.COMPILE, defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class CodeGen extends AbstractMojo {
     
     
     @Parameter( defaultValue = "${project.build.directory}")
     private File outputDirectory;
 
-    @Parameter( defaultValue = "${project.basedir}/src/main/java")
+    @Parameter( defaultValue = "${project.basedir}/target/generated-sources/java")
     private File javaFolder;
+    
+	@Parameter( defaultValue = "${project}")
+	private MavenProject project;
+    
     public void execute() throws MojoExecutionException, MojoFailureException 
     {
+    	
         try {
 
         System.out.println(outputDirectory.getAbsolutePath());
         System.out.println(javaFolder.getAbsolutePath());
-        File file = new File(javaFolder + "/test.java");
-        String data = "public class test {}";
+        File file = new File(javaFolder + "/test/SalesOrder.java");
+        String data = "package test;\npublic class SalesOrder {}";
 		FileUtils.writeStringToFile(file, data, "utf-8");
+		project.addCompileSourceRoot(javaFolder.getPath());
 		} catch (Exception e) 
         {
 			e.printStackTrace();
 			throw new MojoFailureException(e.getMessage());
 		}
+        
     }
 
 }
---
+++ maven-plugin/maven-plugin-test/src/main/java/test/AppMain.java
@@ -0,0 +1,10 @@
+package test;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		SalesOrder a = new SalesOrder();
+
+	}
+
+}
