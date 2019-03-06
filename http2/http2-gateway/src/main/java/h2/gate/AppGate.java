dc7eaaa71670b7e2f3bf2ffca22e7bc4ea6abb49
SparkleX <sunxufei@hotmail.com>
2019-03-06 14:28:23 +0800

    add

---
+++ http2/http2-gateway/src/main/java/h2/gate/AppGate.java
@@ -0,0 +1,17 @@
+package h2.gate;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+public class AppGate
+{
+	public static void main(String[] args)
+	{
+        SpringApplication.run(AppGate.class, args);
+	}
+}
